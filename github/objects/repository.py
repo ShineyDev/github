9f50713eb40510a55a378ea5a8d73e38820f92ba
Riley Shaw <contact@shiney.dev>
2019-07-20 04:37:27 +0100

    implement PartialRepository

--- github/objects/repository.py
+++ github/objects/repository.py
@@ -32,7 +32,7 @@ from github.objects import user
 
 class Repository(abc.Node):
     """
-    Represents a GitHub Repository.
+    Represents a GitHub repository.
 
     https://developer.github.com/v4/object/repository/
     """
@@ -355,5 +355,303 @@ class Repository(abc.Node):
 
         return repositorysubscription.RepositorySubscription.from_data(self.data["viewerSubscription"])
 
-class PartialRepository():
-    ...
+class PartialRepository(abc.Node):
+    """
+    Represents a GitHub repository.
+
+    This partial-class doesn't implement the :attr:`Repository.parent` and :attr:`Repository.template` attributes.
+    """
+
+    __slots__ = ("data", "http")
+
+    def __init__(self, data: dict, http):
+        self.data = data
+        self.http = http
+
+    def __repr__(self) -> str:
+        return "<{0.__class__.__name__} owner='{0.owner.login}' name='{0.name}'>".format(self)
+
+    @classmethod
+    def from_data(cls, data: dict, http) -> "PartialRepository":
+        return cls(data, http)
+
+    @property
+    def allows_merge_commit(self) -> bool:
+        """
+        Whether or not pull requests can be merged with a merge commit on the repository.
+        """
+
+        return self.data["mergeCommitAllowed"]
+
+    @property
+    def allows_rebase_merge(self) -> bool:
+        """
+        Whether or not rebase-merging is enabled on the repository.
+        """
+
+        return self.data["rebaseMergeAllowed"]
+
+    @property
+    def allows_squash_merge(self) -> bool:
+        """
+        Whether or not squash-merging is enabled on the repository.
+        """
+
+        return self.data["squashMergeAllowed"]
+
+    @property
+    def code_of_conduct(self) -> typing.Optional[codeofconduct.CodeOfConduct]:
+        """
+        The repository's code of conduct.
+        """
+
+        codeofconduct_ = self.data["codeOfConduct"]
+        if codeofconduct_:
+            return codeofconduct.CodeOfConduct.from_data(codeofconduct_)
+
+    @property
+    def created_at(self) -> datetime.datetime:
+        """
+        The date and time when the repository was created.
+        """
+
+        return utils.iso_to_datetime(self.data["createdAt"])
+
+    @property
+    def database_id(self) -> int:
+        """
+        The primary key for the repository from the database.
+        """
+
+        return self.data["databaseId"]
+
+    @property
+    def default_branch(self) -> str:
+        """
+        The name of the default branch.
+        """
+
+        return self.data["defaultBranchRef"]["name"]
+
+    @property
+    def description(self) -> str:
+        """
+        The description of the repository.
+        """
+
+        return self.data["description"] or ""
+
+    @property
+    def disk_usage(self) -> int:
+        """
+        The number of kilobytes the repository occupies on disk.
+        """
+
+        return self.data["diskUsage"]
+
+    @property
+    def fork_count(self) -> int:
+        """
+        The number of forks created from the repository.
+        """
+
+        return self.data["forkCount"]
+
+    @property
+    def has_issues(self) -> bool:
+        """
+        Whether or not the repository has issues enabled.
+        """
+
+        return self.data["hasIssuesEnabled"]
+
+    @property
+    def has_wiki(self) -> bool:
+        """
+        Whether or not the repository has wiki enabled.
+        """
+
+        return self.data["hasWikiEnabled"]
+
+    @property
+    def is_archived(self) -> bool:
+        """
+        Whether or not the repository is archived.
+        """
+
+        return self.data["isArchived"]
+
+    @property
+    def is_disabled(self) -> bool:
+        """
+        Whether or not the repository is disabled.
+        """
+
+        return self.data["isDisabled"]
+
+    @property
+    def is_fork(self) -> bool:
+        """
+        Whether or not the repository is a fork of another repository.
+        """
+
+        return self.data["isFork"]
+
+    @property
+    def is_locked(self) -> bool:
+        """
+        Whether or not the repository is locked.
+        """
+
+        return self.data["isLocked"]
+
+    @property
+    def is_mirror(self) -> bool:
+        """
+        Whether or not the repository is a mirror of another repository.
+        """
+
+        return self.data["isMirror"]
+
+    @property
+    def is_private(self) -> bool:
+        """
+        Whether or not the repository is private.
+        """
+
+        return self.data["isPrivate"]
+
+    @property
+    def is_template(self) -> bool:
+        """
+        Whether or not the repository is a template repository.
+        """
+
+        return self.data["isTemplate"]
+
+    @property
+    def license(self) -> typing.Optional[license.License]:
+        """
+        The repository's license.
+        """
+
+        license_ = self.data["licenseInfo"]
+        if license_:
+            return license.License.from_data(license_)
+
+    @property
+    def lock_reason(self) -> typing.Optional[repositorylockreason.RepositoryLockReason]:
+        """
+        The reason for the repository to be in a locked state.
+        """
+
+        lock_reason = self.data["lockReason"]
+        if lock_reason:
+            return repositorylockreason.RepositoryLockReason.from_data(lock_reason)
+
+    @property
+    def name(self) -> str:
+        """
+        The name of the repository.
+        """
+
+        return self.data["name"]
+
+    @property
+    def owner(self) -> typing.Union[user.User]: # add org
+        """
+        The owner of the repository.
+        """
+
+        owner = self.data["owner"]
+
+        if owner["__typename"] == "User":
+            return user.User.from_data(owner, self.http)
+        elif owner["__typename"] == "Organization":
+            ...
+
+    @property
+    def primary_language(self) -> language.Language:
+        """
+        The primary language of the repository.
+        """
+
+        primary_language = self.data["primaryLanguage"]
+        if primary_language:
+            return language.Language.from_data(primary_language)
+
+    @property
+    def pushed_at(self) -> typing.Optional[datetime.datetime]:
+        """
+        The date and time when the repository was last pushed to.
+        """
+
+        pushed_at = self.data["pushedAt"]
+        if pushed_at:
+            return utils.iso_to_datetime(pushed_at)
+
+    @property
+    def updated_at(self) -> typing.Optional[datetime.datetime]:
+        """
+        The date and time when the repository was last updated.
+        """
+
+        updated_at = self.data["updatedAt"]
+        if updated_at:
+            return utils.iso_to_datetime(updated_at)
+
+    @property
+    def url(self) -> str:
+        """
+        The repository's url.
+        """
+
+        return self.data["url"]
+
+    @property
+    def viewer_can_administer(self) -> bool:
+        """
+        Whether or not the authenticated user can administer the repository.
+        """
+
+        return self.data["viewerCanAdminister"]
+
+    @property
+    def viewer_can_create_projects(self) -> bool:
+        """
+        Whether or not the authenticated user can create projects in the repository.
+        """
+
+        return self.data["viewerCanCreateProjects"]
+
+    @property
+    def viewer_can_subscribe(self) -> bool:
+        """
+        Whether or not the authenticated user can subscribe to the repository.
+        """
+
+        return self.data["viewerCanSubscribe"]
+
+    @property
+    def viewer_can_update_topics(self) -> bool:
+        """
+        Whether or not the authenticated user can update topics in the repository.
+        """
+
+        return self.data["viewerCanUpdateTopics"]
+
+    @property
+    def viewer_permissions(self) -> repositorypermissions.RepositoryPermissions:
+        """
+        The authenticated user's permissions in the repository.
+        """
+
+        return repositorypermissions.RepositoryPermissions.from_data(self.data["viewerPermission"])
+
+    @property
+    def viewer_subscription(self) -> repositorysubscription.RepositorySubscription:
+        """
+        The authenticated user's subscription to the repository.
+        """
+
+        return repositorysubscription.RepositorySubscription.from_data(self.data["viewerSubscription"])
