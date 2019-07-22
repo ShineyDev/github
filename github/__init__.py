30b6c5eb57ddcdec497dd18c18889e7ebc755fa7
Riley Shaw <contact@shiney.dev>
2019-07-22 09:20:14 +0100

    bump version to 1.0.1-final

--- github/__init__.py
+++ github/__init__.py
@@ -24,7 +24,7 @@ from github.errors import GitHubError
 from github.github import GitHub
 
 
-__version__ = "0.1.0a"
+__version__ = "0.1.0"
 
 VersionInfo = collections.namedtuple("VersionInfo", "major minor micro releaselevel")
-version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel="alpha")
+version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel="final")
