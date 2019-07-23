ce47849a6394ee1090275dac048c0e35e9738fa0
Riley Shaw <contact@shiney.dev>
2019-07-23 01:52:15 +0100

    bump version to 0.1.1

--- github/__init__.py
+++ github/__init__.py
@@ -24,7 +24,7 @@ from github.errors import GitHubError
 from github.github import GitHub
 
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 VersionInfo = collections.namedtuple("VersionInfo", "major minor micro releaselevel")
 version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel="final")
