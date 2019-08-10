ac6f13c41b20e80615faf73a7f435e14f30cda8d
Riley Shaw <contact@shiney.dev>
2019-08-10 18:12:04 +0100

    implement Collection.from_dict, .to_dict & .copy

--- github/query/builder.py
+++ github/query/builder.py
@@ -350,6 +350,45 @@ class Collection():
         self._collections = list()
         self._fields = list()
 
+    @classmethod
+    def from_dict(cls, data: dict) -> "Collection":
+        """
+        Creates a :class:`~github.query.Collection` object from a dict.
+
+        Parameters
+        ----------
+        data: :class:`dict`
+            The dict to convert into a
+            :class:`~github.query.Collection`.
+
+        Returns
+        -------
+        :class:`github.query.Collection`
+            The :class:`~github.query.Collection` object.
+        """
+
+        name = data["name"]
+        alias = data.get("alias", None)
+
+        collection = cls(name=name, alias=alias)
+        
+        arguments = data.get("arguments", list())
+        for (argument) in arguments:
+            argument = CollectionArgument.from_dict(argument)
+            collection.add_argument(argument)
+        
+        collections = data.get("collections", list())
+        for (collection) in collections:
+            collection = Collection.from_dict(collection)
+            collection.add_collection(collection)
+        
+        fields = data.get("fields", list())
+        for (field) in fields:
+            field = Field.from_dict(field)
+            collection.add_field(fields)
+
+        return collection
+
     @property
     def arguments(self) -> typing.List["CollectionArgument"]:
         """
@@ -490,6 +529,47 @@ class Collection():
         # }
 
         return collection
+
+    def copy(self) -> "Collection":
+        """
+        Creates a shallow-copy of this object.
+
+        Returns
+        -------
+        :class:`github.query.Collection`
+            The new object.
+        """
+
+        return Collection.from_dict(self.to_dict())
+
+    def to_dict(self) -> dict:
+        """
+        Creates a dict object from this
+        :class:`~github.query.Collection`.
+
+        Returns
+        -------
+        :class:`dict`
+            The dict object.
+        """
+
+        data = dict()
+
+        data["name"] = self.name
+
+        if self.alias is not None:
+            data["alias"] = self.alias
+
+        if self._arguments:
+            data["arguments"] = [a.to_dict() for a in self._arguments]
+
+        if self._collections:
+            data["collections"] = [c.to_dict() for c in self._collections]
+
+        if self._fields:
+            data["fields"] = [f.to_dict() for f in self._fields]
+
+        return data
 
 class CollectionArgument():
     """
