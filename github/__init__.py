bb68c8064bb5228d0cb35c4da675e801bd9a02c4
Riley Shaw <contact@shiney.dev>
2019-08-17 00:02:13 +0100

    bump version to 0.3.0

--- github/__init__.py
+++ github/__init__.py
@@ -25,7 +25,7 @@ from github.objects import *
 from github.github import GitHub
 
 
-__version__ = "0.3.0a"
+__version__ = "0.3.0"
 
 VersionInfo = collections.namedtuple("VersionInfo", "major minor micro releaselevel")
-version_info = VersionInfo(major=0, minor=3, micro=0, releaselevel="alpha")
+version_info = VersionInfo(major=0, minor=3, micro=0, releaselevel="final")
