54b5051726d6a7913a2ac2789f6830c821b9e096
Riley Shaw <contact@shiney.dev>
2019-08-17 06:15:17 +0100

    update `github --version` display

--- github/__main__.py
+++ github/__main__.py
@@ -29,17 +29,23 @@ import github
 def show_version():
     entries = list()
 
-    entries.append("- python v{0.major}.{0.minor}.{0.micro}-{0.releaselevel}".format(sys.version_info))
+    if sys.version_info.releaselevel != "final":
+        entries.append("- python: v{0.major}.{0.minor}.{0.micro}{0.releaselevel[0]}{0.serial}".format(sys.version_info))
+    else:
+        entries.append("- python: v{0.major}.{0.minor}.{0.micro}".format(sys.version_info))
 
-    entries.append("- github.py v{0.major}.{0.minor}.{0.micro}-{0.releaselevel}".format(github.version_info))
     if github.version_info.releaselevel != "final":
+        entries.append("- github.py: v{0.major}.{0.minor}.{0.micro}{0.releaselevel[0]}{0.serial}".format(github.version_info))
+
         pkg = pkg_resources.get_distribution("github.py")
         if pkg:
             entries.append("    - package: v{0}".format(pkg.version))
+    else:
+        entries.append("- github.py: v{0.major}.{0.minor}.{0.micro}".format(github.version_info))
 
-    entries.append("- aiohttp v{0.__version__}".format(aiohttp))
+    entries.append("- aiohttp: v{0.__version__}".format(aiohttp))
     entries.append("")
-    entries.append("- system info: {0.system} {0.version}".format(platform.uname()))
+    entries.append("- system: {0.system} {0.version}".format(platform.uname()))
 
     print("\n".join(entries))
 
