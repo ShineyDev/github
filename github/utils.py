4b34a72ba3a90071e650ad54a81f19a335b8e5fc
Riley Shaw <contact@shiney.dev>
2020-12-02 13:14:22 +0000

    update utils

--- github/utils.py
+++ github/utils.py
@@ -16,73 +16,56 @@
     limitations under the License.
 """
 
+import base64
 import datetime
 import inspect
-import re
+import struct
 
 
-# https://docs.github.com/en/graphql/reference/scalars#datetime
-# https://docs.github.com/en/graphql/reference/scalars#precisedatetime
-ISO_8601_DATETIME_FMT = "{0:>04}-{1:>02}-{2:>02}T{3:>02}:{4:>02}:{5:>02}.{6:>03}Z"
-ISO_8601_DATETIME_REGEX = r"([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2}):([0-9]{2})(?:\.([0-9]{3}))?Z"
+def cursor_to_database(cursor):
+    cursor = base64.b64decode(cursor)
+    name, version, msgpack = cursor.split(b":")
+    return struct.unpack_from(">I", msgpack, 2)[0]
 
+def database_to_cursor(id):
+    msgpack = b"\x91\xCE" + struct.pack(">I", id)
+    cursor = b"cursor:v2:" + msgpack
+    return base64.b64encode(cursor)
 
 def datetime_to_iso(dt):
-    """
-    Converts a datetime object to ISO-8601.
+    offset = dt.utcoffset()
+    if not offset:
+        offset = "Z"
+    else:
+        if offset < datetime.timedelta(0):
+            sign = "-"
+            offset = -offset
+        else:
+            sign = "+"
 
-    Parameters
-    ----------
-    dt: :class:`~datetime.datetime`
-        A datetime. object.
+        hours, rest = divmod(offset, datetime.timedelta(hours=1))
+        minutes, _ = divmod(rest, datetime.timedelta(minutes=1))
 
-    Returns
-    -------
-    :class:`str`
-        An ISO-8601 string.
-    """
+        offset = f"{sign}{hours:>02}:{minutes:>02}"
 
-    return ISO_8601_DATETIME_FMT.format(dt.year, dt.month, dt.day,
-                                        dt.hour, dt.minute, dt.second,
-                                        dt.microsecond // 1000)
+    return dt.strftime(f"%Y-%m-%dT%H:%M:%S{offset}")
 
 def iso_to_datetime(iso):
-    """
-    Converts ISO-8601 to a datetime object.
-
-    Parameters
-    ----------
-    iso: :class:`str`
-        An ISO-8601 string.
-
-    Returns
-    -------
-    :class:`~datetime.datetime`
-        A datetime object.
-    """
-
     if iso is None:
-        # internal helper
         return None
 
-    match = re.fullmatch(ISO_8601_DATETIME_REGEX, iso)
-    if match:
-        year = int(match.group(1))
-        month = int(match.group(2))
-        day = int(match.group(3))
-        hour = int(match.group(4))
-        minute = int(match.group(5))
-        second = int(match.group(6))
-        microsecond = int(match.group(7) or 0) * 1000
+    return datetime.datetime.fromisoformat(iso.replace("Z", "+00:00"))
 
-        return datetime.datetime(year, month, day,
-                                 hour, minute, second,
-                                 microsecond)
+def iso_to_date(iso):
+    if iso is None:
+        return None
+
+    return datetime.date.fromisoformat(iso)
 
 async def maybe_coro(callable, *args, **kwargs):
-    coro = callable(*args, **kwargs)
+    result = callable(*args, **kwargs)
 
-    if inspect.iscoroutine(coro):
-        return await coro
+    if inspect.iscoroutine(result):
+        result = await result
 
-    return coro
+    return result
