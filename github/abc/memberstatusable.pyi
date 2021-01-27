d761159ed276f1a674fcbc699862acaa095deebe
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-01-27 21:38:09 +0000

    update memberstatusable.pyi

--- github/abc/memberstatusable.pyi
+++ github/abc/memberstatusable.pyi
@@ -1,6 +1,7 @@
 from github.iterator import CollectionIterator
+from github.enums import UserStatusOrderField
 from github.objects import UserStatus
 
 
 class MemberStatusable():
-    def fetch_member_statuses(self, **kwargs) -> CollectionIterator[UserStatus]: ...
+    def fetch_member_statuses(self, order_by: UserStatusOrderField=..., **kwargs) -> CollectionIterator[UserStatus]: ...
