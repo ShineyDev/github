269824cf653b0cf4c39115c9891befa5a5af12a3
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-01-27 21:53:05 +0000

    abstract Commentable.fetch_comments typings

--- github/abc/commentable.pyi
+++ github/abc/commentable.pyi
@@ -1,22 +1,5 @@
-from typing import Union
-
-from github.iterator import CollectionIterator
-from github.objects import CommitComment
-from github.objects import GistComment
-from github.objects import IssueComment
-from github.objects import PullRequestComment
-from github.objects import PullRequestReviewComment
-from github.objects import TeamDiscussionComment
+from typing import Any
 
 
 class Commentable():
-    def fetch_comments(
-        self, **kwargs
-    ) -> Union[
-        CollectionIterator[CommitComment],
-        CollectionIterator[GistComment],
-        CollectionIterator[IssueComment],
-        CollectionIterator[PullRequestComment],
-        CollectionIterator[PullRequestReviewComment],
-        CollectionIterator[TeamDiscussionComment],
-    ]: ...
+    def fetch_comments(self, **kwargs) -> Any: ...
