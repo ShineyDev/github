ede161224c8f9ada8126eddcb39cd07f7cf05b33
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-05-07 14:09:30 +0100

    sort imports

--- github/__init__.py
+++ github/__init__.py
@@ -4,17 +4,17 @@ from github.client import *
 from github.client import __all__ as _client__all__
 from github.errors import *
 from github.errors import __all__ as _errors__all__
-from github.content import *
-from github.content import __all__ as _content__all__
 from github.interfaces import *
 from github.interfaces import __all__ as _interfaces__all__
+from github.content import *
+from github.content import __all__ as _content__all__
 
 
 __all__ = [
     *_client__all__,
     *_errors__all__,
-    *_content__all__,
     *_interfaces__all__,
+    *_content__all__,
 ]
 
 
