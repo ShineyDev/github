36a672b50f70f3a424c92ccd207b2c3e5f4b9c82
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-07-19 13:53:18 +0100

    patch CodeOfConduct stub

--- github/content/codeofconduct.pyi
+++ github/content/codeofconduct.pyi
@@ -1,10 +1,18 @@
-from github.interfaces import Node, Type, UniformResourceLocatable
+from typing import Optional
+
+from github.interfaces import Node, Type
 
 
-class CodeOfConduct(Node, Type, UniformResourceLocatable):
+class CodeOfConduct(Node, Type):
     @property
     def body(self) -> str: ...
     @property
     def key(self) -> str: ...
     @property
     def name(self) -> str: ...
+
+    # patch for UniformResourceLocatable inheritance
+    @property
+    def resource_path(self) -> Optional[str]: ...
+    @property
+    def url(self) -> Optional[str]: ...
