72c6f67990cae83122bda3d437a3a464fa1afe26
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-09-01 11:33:44 +0100

    patch Client stub

--- github/client.pyi
+++ github/client.pyi
@@ -4,7 +4,7 @@ import graphql
 from github.content import CodeOfConduct
 
 
-class Client:
+class Client(graphql.client.Client):
     def __init__(self, token: str, *, session: aiohttp.ClientSession, user_agent: str=...) -> None: ...
 
     async def fetch_all_codes_of_conduct(self, **kwargs) -> list[CodeOfConduct]: ...
