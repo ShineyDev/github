796b9a3fca808a0dfd836aa3c51ae7ac771ea441
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-11-16 13:21:25 +0000

    patch lint workflow
    fuck you black

--- github/content/codeofconduct.py
+++ github/content/codeofconduct.py
@@ -25,11 +25,11 @@ class CodeOfConduct(Node, Type, UniformResourceLocatable):
         "key",
     ]
 
-    _graphql_fields = (
+    _graphql_fields = [
         "body",
         "key",
         "name",
-    )
+    ]
 
     @property
     def body(self):
