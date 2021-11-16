4eb1c549c739a176d6aa394bae26225ea9be8ef3
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-11-16 12:12:24 +0000

    update documentation configuration

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,34 @@
-import os
 import re
-import sys
 
-sys.path.insert(0, os.path.abspath(".."))
+
+author = "ShineyDev"
+project = "github"
+
+copyright = f"2019-present, {author}"
+
+_version_regex = r"^version = ('|\")((?:[0-9]+\.)*[0-9]+(?:\.?([a-z]+)(?:\.?[0-9])?)?)\1$"
+
+with open("../github/__init__.py") as stream:
+    match = re.search(_version_regex, stream.read(), re.MULTILINE)
+
+release = "v" + match.group(2)
+version = release
+
+if match.group(3) is not None:
+    try:
+        import subprocess
+
+        process = subprocess.Popen(["git", "rev-list", "--count", "HEAD"], stdout=subprocess.PIPE)
+        out, _ = process.communicate()
+        if out:
+            release += out.decode("utf-8").strip()
+
+        process = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE)
+        out, _ = process.communicate()
+        if out:
+            release += "+g" + out.decode("utf-8").strip()
+    except (Exception) as e:
+        pass
 
 
 extensions = [
@@ -18,7 +44,7 @@ autodoc_member_order = "groupwise"
 autodoc_typehints = "none"
 
 extlinks = {
-    "issue": ("https://github.com/ShineyDev/github/issues/%s", "#"),
+    "issue": (f"https://github.com/{author}/{project}/issues/%s", "#"),
 }
 
 intersphinx_mapping = {
@@ -27,46 +53,10 @@ intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
 }
 
-html_theme = "sphinx_rtd_theme"
-html_theme_options = {
-    "collapse_navigation": False,
-    "includehidden": False,
-    "navigation_depth": -1,
-    "prev_next_buttons_location": None,
-    "titles_only": True,
-}
-master_doc = "index"
+
+highlight_language = "none"
 pygments_style = "friendly"
-source_suffix = ".rst"
-
-copyright = "2019-present, ShineyDev"
-project = "github"
-
-_version_regex = r"^version = ('|\")((?:[0-9]+\.)*[0-9]+(?:\.?([a-z]+)(?:\.?[0-9])?)?)\1$"
-
-with open("../github/__init__.py") as stream:
-    match = re.search(_version_regex, stream.read(), re.MULTILINE)
-
-version = match.group(2)
-
-if match.group(3) is not None:
-    try:
-        import subprocess
-
-        process = subprocess.Popen(["git", "rev-list", "--count", "HEAD"], stdout=subprocess.PIPE)
-        out, _ = process.communicate()
-        if out:
-            version += out.decode("utf-8").strip()
-
-        process = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE)
-        out, _ = process.communicate()
-        if out:
-            version += "+g" + out.decode("utf-8").strip()
-    except (Exception) as e:
-        pass
-
-release = version
-
+root_doc = "index"
 rst_prolog = """
 
 .. |aiter| replace:: This function returns an |aiter_link|_.
@@ -92,3 +82,15 @@ rst_prolog = """
 .. |PAT_link| replace:: personal access token
 .. _PAT_link: https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token
 """
+source_suffix = ".rst"
+
+
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    "collapse_navigation": False,
+    "includehidden": False,
+    "navigation_depth": -1,
+    "prev_next_buttons_location": None,
+    "titles_only": True,
+}
+html_title = f"{project} {version}"
