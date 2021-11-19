aaf989f5e4552ccdef8eb7d89b3d05acc12ba88c
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-11-19 11:31:21 +0000

    sort errors

--- github/errors.py
+++ github/errors.py
@@ -24,28 +24,6 @@ class ClientResponseError(graphql.client.ClientResponseError, ClientError):
     __slots__ = ()
 
 
-class ClientResponseHTTPError(graphql.client.ClientResponseHTTPError, ClientResponseError):
-    __doc__ = graphql.client.ClientResponseHTTPError.__doc__
-    __slots__ = ()
-
-
-class ClientResponseHTTPUnauthorizedError(ClientResponseHTTPError):
-    """
-    Represents an HTTP 401 response.
-
-    Attributes
-    ----------
-    message: :class:`str`
-        The error message.
-    response: :class:`aiohttp.ClientResponse`
-        The client response.
-    data: Optional[:class:`dict`]
-        The response data.
-    """
-
-    __slots__ = ()
-
-
 class ClientResponseGraphQLError(graphql.client.ClientResponseGraphQLError, ClientResponseError):
     __doc__ = graphql.client.ClientResponseGraphQLError.__doc__
     __slots__ = ()
@@ -136,12 +114,34 @@ class ClientResponseGraphQLValidationError(graphql.client.ClientResponseGraphQLV
     __slots__ = ()
 
 
+class ClientResponseHTTPError(graphql.client.ClientResponseHTTPError, ClientResponseError):
+    __doc__ = graphql.client.ClientResponseHTTPError.__doc__
+    __slots__ = ()
+
+
+class ClientResponseHTTPUnauthorizedError(ClientResponseHTTPError):
+    """
+    Represents an HTTP 401 response.
+
+    Attributes
+    ----------
+    message: :class:`str`
+        The error message.
+    response: :class:`aiohttp.ClientResponse`
+        The client response.
+    data: Optional[:class:`dict`]
+        The response data.
+    """
+
+    __slots__ = ()
+
+
 _response_error_map = {
-    401: ClientResponseHTTPUnauthorizedError,
     "FORBIDDEN": ClientResponseGraphQLForbiddenError,
     "INTERNAL": ClientResponseGraphQLInternalError,
     "NOT_FOUND": ClientResponseGraphQLNotFoundError,
     "UNPROCESSABLE": ClientResponseGraphQLUnprocessableError,
+    401: ClientResponseHTTPUnauthorizedError,
 }
 
 
@@ -165,14 +165,14 @@ __all__ = [
     "ClientError",
     "ClientObjectMissingFieldError",
     "ClientResponseError",
-    "ClientResponseHTTPError",
-    "ClientResponseHTTPUnauthorizedError",
     "ClientResponseGraphQLError",
     "ClientResponseGraphQLForbiddenError",
     "ClientResponseGraphQLInternalError",
     "ClientResponseGraphQLNotFoundError",
     "ClientResponseGraphQLUnprocessableError",
     "ClientResponseGraphQLValidationError",
+    "ClientResponseHTTPError",
+    "ClientResponseHTTPUnauthorizedError",
     "ClientDeprecationWarning",
     "ServerDeprecationWarning",
 ]
