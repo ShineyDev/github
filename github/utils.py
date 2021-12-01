4a82d1581f9c23f5e3aee38930a00aa3a0e6f572
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-12-01 20:42:45 +0000

    remove old node id transforms

--- github/utils.py
+++ github/utils.py
@@ -55,14 +55,17 @@ def iso_to_date(iso):
     return datetime.date.fromisoformat(iso)
 
 
+# TODO: use next ids
+
+
 def _cursor_to_database(cursor):
     cursor = base64.b64decode(cursor)
     name, version, msgpack = cursor.split(b":")
     return struct.unpack_from(">I", msgpack, 2)[0]
 
 
-def _cursor_to_node(cursor, type):
-    return _database_to_node(_cursor_to_database(cursor), type)
+# def _cursor_to_node(cursor, type):
+#     return _database_to_node(_cursor_to_database(cursor), type)
 
 
 def _database_to_cursor(id):
@@ -70,27 +73,27 @@ def _database_to_cursor(id):
     return base64.b64encode(cursor).decode("utf-8")
 
 
-def _database_to_node(id, type):
-    return base64.b64encode(f"0{len(type)}:{type}{id}".encode("utf-8")).decode("utf-8")
-
-
-def _node_to_cursor(id):
-    return _database_to_cursor(_node_to_database(id))
-
-
-_bad_types = frozenset({"CodeOfConduct", "Gist"})
-
-
-def _node_to_database(id):
-    id = base64.b64decode(id).decode("utf-8")[1:]
-    length, id = id.split(":")
-    length = int(length)
-    type, id = id[:length], id[length:]
-
-    if type in _bad_types:
-        raise ValueError(f"node id of type '{type}' does not contain database id, got {id}")
-
-    return int(id)
+# def _database_to_node(id, type):
+#     return base64.b64encode(f"0{len(type)}:{type}{id}".encode("utf-8")).decode("utf-8")
+#
+#
+# def _node_to_cursor(id):
+#     return _database_to_cursor(_node_to_database(id))
+#
+#
+# _bad_types = frozenset({"CodeOfConduct", "Gist"})
+#
+#
+# def _node_to_database(id):
+#     id = base64.b64decode(id).decode("utf-8")[1:]
+#     length, id = id.split(":")
+#     length = int(length)
+#     type, id = id[:length], id[length:]
+#
+#     if type in _bad_types:
+#         raise ValueError(f"node id of type '{type}' does not contain database id, got {id}")
+#
+#     return int(id)
 
 
 _empty_dict = dict()
