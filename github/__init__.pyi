fbc7a3f66af34350c754e2d8b278ef419d0296b9
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2022-01-04 20:38:32 +0000

    whoops

--- github/__init__.pyi
+++ github/__init__.pyi
@@ -1,6 +1,6 @@
 from typing import NamedTuple
 
-from github import utils
+from github import utils as utils
 from github.client import *
 from github.errors import *
 from github.interfaces import *
