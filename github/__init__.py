d431229fd061cbae7046c35640424bfb0face172
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2024-09-20 22:23:51 +0100

    sort github.__init__ imports

--- github/__init__.py
+++ github/__init__.py
@@ -3,12 +3,12 @@ from typing import NamedTuple
 
 from github.core import *
 from github.core import __all__ as _core__all__
-from github.content import *
-from github.content import __all__ as _content__all__
 from github.interfaces import *
 from github.interfaces import __all__ as _interfaces__all__
 from github.connection import *
 from github.connection import __all__ as _connection__all__
+from github.content import *
+from github.content import __all__ as _content__all__
 from github.repository import *
 from github.repository import __all__ as _repository__all__
 from github.user import *
@@ -32,8 +32,8 @@ version_info: _VersionInfo = _VersionInfo(1, 0, 0, 0, "alpha", 0)
 __all__: list[str] = [  # type: ignore[reportUnsupportedDunderAll]
     *_core__all__,
     *_interfaces__all__,
-    *_content__all__,
     *_connection__all__,
+    *_content__all__,
     *_repository__all__,
     *_user__all__,
     "utility",
