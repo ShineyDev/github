d917fcaca89e5029e4f4d321b8cfc6ac5a35915b
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2024-09-25 21:01:22 +0100

    implement Announcement

--- github/content/__init__.py
+++ github/content/__init__.py
@@ -1,3 +1,5 @@
+from github.content.announcement import *
+from github.content.announcement import __all__ as _announcement__all__
 from github.content.codeofconduct import *
 from github.content.codeofconduct import __all__ as _codeofconduct__all__
 from github.content.license import *
@@ -7,6 +9,7 @@ from github.content.licenserule import __all__ as _licenserule__all__
 
 
 __all__: list[str] = [  # type: ignore[reportUnsupportedDunderAll]
+    *_announcement__all__,
     *_codeofconduct__all__,
     *_license__all__,
     *_licenserule__all__,
---
+++ github/content/announcement.py
@@ -0,0 +1,243 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing_extensions import Self, cast
+
+    from github.core.http import HTTPClient
+    from github.utility.types import T_json_key, T_json_value, DateTime
+
+import github
+from github.core.errors import ClientObjectMissingFieldError
+from github.interfaces import Type
+from github.utility import MISSING
+
+
+if TYPE_CHECKING:
+    from github.interfaces.type import TypeData
+
+
+    class AnnouncementData(TypeData):
+        announcement: str
+        announcementCreatedAt: str
+        announcementExpiresAt: str | None
+        announcementUserDismissable: bool
+        id: str
+
+
+class Announcement(Type):
+    """
+    Represents an announcement banner on an Enterprise or Organization.
+
+
+    .. container:: operations
+
+        .. describe:: x == y
+        .. describe:: x != y
+
+            Compares two objects by their :attr:`ID <.id>`.
+
+        .. describe:: hash(x)
+
+            Returns the hash of the object's :attr:`ID <.id>`.
+    """
+
+    __slots__ = ()
+
+    _data: AnnouncementData
+    _http: HTTPClient
+
+    _graphql_fields: dict[str, str] = {
+        "text": "announcement",
+        "created_at": "announcementCreatedAt",
+        "expires_at": "announcementExpiresAt",
+        "can_viewer_dismiss": "announcementUserDismissable",
+        "id": "id",
+    }
+
+    @property
+    def can_viewer_dismiss(
+        self: Self,
+        /,
+    ) -> bool:
+        """
+        Whether the authenticated user can dismiss the announcement.
+
+        :type: :class:`bool`
+        """
+
+        return self._data["announcementUserDismissable"]
+
+    @property
+    def created_at(
+        self: Self,
+        /,
+    ) -> DateTime:
+        """
+        The date and time at which the announcement was created.
+
+        :type: :class:`~datetime.datetime`
+        """
+
+        return github.utility.iso_to_datetime(self._data["announcementCreatedAt"])
+
+    @property
+    def expires_at(
+        self: Self,
+        /,
+    ) -> DateTime | None:
+        """
+        The date and time at which the announcement will expire.
+
+        :type: Optional[:class:`~datetime.datetime`]
+        """
+
+        expires_at = self._data["announcementExpiresAt"]
+
+        if expires_at is None:
+            return None
+
+        return github.utility.iso_to_datetime(expires_at)
+
+    @property
+    def text(
+        self: Self,
+        /,
+    ) -> str:
+        """
+        The text of the announcement.
+
+        :type: :class:`str`
+        """
+
+        return self._data["announcement"]
+
+    async def _fetch_field(
+        self: Self,
+        field: T_json_key,
+        /,
+        *,
+        save: bool = MISSING,
+    ) -> T_json_value:
+        try:
+            id = self._data["id"]
+        except KeyError:
+            raise ClientObjectMissingFieldError from None
+
+        data = await self._http.fetch_query_node(self.__class__, id, fields=(field,))
+
+        value = data[field]
+
+        if save:
+            self._data[field] = value
+
+        return value
+
+    async def fetch_can_viewer_dismiss(
+        self: Self,
+        /,
+    ) -> str:
+        """
+        |coro|
+
+        Fetches whether the authenticated user can dismiss the
+        announcement.
+
+
+        Raises
+        ------
+
+        ~github.core.errors.ClientObjectMissingFieldError
+            The :attr:`id` attribute is missing.
+
+
+        :rtype: :class:`bool`
+        """
+
+        return await self._fetch_field("announcementUserDismissable")  # type: ignore
+
+    async def fetch_created_at(
+        self: Self,
+        /,
+    ) -> DateTime:
+        """
+        |coro|
+
+        Fetches the date and time at which the announcement was
+        created.
+
+
+        Raises
+        ------
+
+        ~github.core.errors.ClientObjectMissingFieldError
+            The :attr:`id` attribute is missing.
+
+
+        :rtype: :class:`~datetime.datetime`
+        """
+
+        created_at = await self._fetch_field("announcementCreatedAt")
+
+        if TYPE_CHECKING:
+            created_at = cast(str, created_at)
+
+        return github.utility.iso_to_datetime(created_at)
+
+    async def fetch_expires_at(
+        self: Self,
+        /,
+    ) -> DateTime | None:
+        """
+        |coro|
+
+        Fetches the date and time at which the announcement will
+        expire.
+
+
+        Raises
+        ------
+
+        ~github.core.errors.ClientObjectMissingFieldError
+            The :attr:`id` attribute is missing.
+
+
+        :rtype: Optional[:class:`~datetime.datetime`]
+        """
+
+        expires_at = await self._fetch_field("announcementExpiresAt")
+
+        if expires_at is None:
+            return None
+
+        if TYPE_CHECKING:
+            expires_at = cast(str, expires_at)
+
+        return github.utility.iso_to_datetime(expires_at)
+
+    async def fetch_text(
+        self: Self,
+        /,
+    ) -> DateTime | None:
+        """
+        |coro|
+
+        Fetches the text of the announcement.
+
+
+        Raises
+        ------
+
+        ~github.core.errors.ClientObjectMissingFieldError
+            The :attr:`id` attribute is missing.
+
+
+        :rtype: :class:`str`
+        """
+
+        return await self._fetch_field("announcement")  # type: ignore
+
+
+__all__: list[str] = [
+    "Announcement",
+]
