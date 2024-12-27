518fa0791ada65a82a2e97d779edc7f0d40d5497
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2024-12-27 16:55:05 +0000

    implement RepositoryOrder

---
+++ github/connections/repositoryorder.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+import enum
+
+
+class RepositoryOrder(enum.Enum):
+    """
+    Represents fields by which you can order :class:`repositories <github.Repository>`.
+    """
+
+    #: The date and time at which the repository was created.
+    created_at = "CREATED_AT"
+
+    #: The name of the repository.
+    name = "NAME"
+
+    #: The date and time at which the repository was last pushed.
+    pushed_at = "PUSHED_AT"
+
+    #: The number of stars on the repository.
+    stargazer_count = "STARGAZERS"
+
+    #: The date and time at which the repository was last updated.
+    updated_at = "UPDATED_AT"
+
+
+__all__ = [
+    "RepositoryOrder"
+]
