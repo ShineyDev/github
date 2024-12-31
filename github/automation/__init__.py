f484bed32f3b73a93eeea0c2c46d9751311ed1e2
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2024-12-31 13:16:14 +0000

    implement Mannequin

--- github/automation/__init__.py
+++ github/automation/__init__.py
@@ -1,7 +1,10 @@
 from github.automation.bot import *
 from github.automation.bot import __all__ as _bot__all__
+from github.automation.mannequin import *
+from github.automation.mannequin import __all__ as _mannequin__all__
 
 
 __all__: list[str] = [  # type: ignore[reportUnsupportedDunderAll]
     *_bot__all__,
+    *_mannequin__all__,
 ]
---
+++ github/automation/mannequin.py
@@ -0,0 +1,101 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing_extensions import Self
+
+    from github.utility.types import DateTime
+
+import github
+from github.interfaces import Actor, Node, Resource, Type
+
+
+if TYPE_CHECKING:
+    from github.interfaces.actor import ActorData
+    from github.interfaces.node import NodeData
+    from github.interfaces.resource import ResourceData
+    from github.interfaces.type import TypeData
+    from github.user.user import User
+
+
+    class MannequinData(ActorData, NodeData, ResourceData, TypeData):
+        claimant: User | None
+        createdAt: str
+        databaseId: int
+        email: str | None
+        updatedAt: str
+
+
+class Mannequin(Actor, Node, Resource, Type):
+    """
+    Represents a placeholder user.
+
+
+    .. container:: operations
+
+        .. describe:: x == y
+        .. describe:: x != y
+
+            Compares two objects by their :attr:`ID <.id>`.
+
+        .. describe:: hash(x)
+
+            Returns the hash of the object's :attr:`ID <.id>`.
+    """
+
+    __slots__ = ()
+
+    _data: MannequinData
+
+    _graphql_fields: dict[str, str] = {
+        "created_at": "createdAt",
+        "database_id": "databaseId",
+        # "email": "email",  # TODO: find
+        "updated_at": "updatedAt",
+    }
+
+    _node_prefix: str = "M"
+
+    @property
+    def created_at(
+        self: Self,
+        /,
+    ) -> DateTime:
+        """
+        The date and time at which the mannequin was created.
+
+        :type: :class:`~datetime.datetime`
+        """
+
+        return github.utility.iso_to_datetime(self._data["createdAt"])
+
+    @property
+    def database_id(
+        self: Self,
+        /,
+    ) -> int:
+        """
+        The database ID of the mannequin.
+
+        :type: :class:`int`
+        """
+
+        return self._data["databaseId"]
+
+    @property
+    def updated_at(
+        self: Self,
+        /,
+    ) -> DateTime:
+        """
+        The date and time at which the mannequin was last updated.
+
+        :type: :class:`~datetime.datetime`
+        """
+
+        return github.utility.iso_to_datetime(self._data["updatedAt"])
+
+
+__all__ = [
+    "Mannequin",
+]
