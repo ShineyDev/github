e969e15596f4ad42ca9607daf3a1cd2d62a06bf2
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2024-12-31 13:57:23 +0000

    add field fetches to Comment
    whoops

--- github/interfaces/comment.py
+++ github/interfaces/comment.py
@@ -2,8 +2,10 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from typing import cast
     from typing_extensions import Self
 
+    from github.interfaces import Node
     from github.utility.types import DateTime
 
 import github
@@ -170,6 +172,189 @@ class Comment:
         """
 
         return self._data["viewerDidAuthor"]
+
+    async def fetch_body(
+        self: Self,
+        /,
+    ) -> str | None:
+        """
+        |coro|
+
+        Fetches the body of the comment.
+
+        :rtype: :class:`str` | None
+        """
+
+        return await self._fetch_field("body")  # type: ignore
+
+    async def fetch_body_html(
+        self: Self,
+        /,
+    ) -> str | None:
+        """
+        |coro|
+
+        Fetches the body of the comment as HTML.
+
+        :rtype: :class:`str` | None
+        """
+
+        return await self._fetch_field("bodyHTML")  # type: ignore
+
+    async def fetch_body_text(
+        self: Self,
+        /,
+    ) -> str | None:
+        """
+        |coro|
+
+        Fetches the body of the comment as text.
+
+        :rtype: :class:`str` | None
+        """
+
+        return await self._fetch_field("bodyText")  # type: ignore
+
+    async def fetch_created_at(
+        self: Self,
+        /,
+    ) -> DateTime:
+        """
+        |coro|
+
+        Fetches the date and time at which the comment was created.
+
+
+        Raises
+        ------
+
+        ~github.core.errors.ClientObjectMissingFieldError
+            The :attr:`id` attribute is missing.
+
+
+        :rtype: :class:`~datetime.datetime`
+        """
+
+        if TYPE_CHECKING and not isinstance(self, Node):
+            raise NotImplementedError
+
+        created_at = await self._fetch_field("createdAt")
+
+        if TYPE_CHECKING:
+            created_at = cast(str, created_at)
+
+        return github.utility.iso_to_datetime(created_at)
+
+    async def fetch_edited_at(
+        self: Self,
+        /,
+    ) -> DateTime | None:
+        """
+        |coro|
+
+        Fetches the date and time at which the comment was last edited.
+
+
+        Raises
+        ------
+
+        ~github.core.errors.ClientObjectMissingFieldError
+            The :attr:`id` attribute is missing.
+
+
+        :rtype: :class:`~datetime.datetime` | None
+        """
+
+        if TYPE_CHECKING and not isinstance(self, Node):
+            raise NotImplementedError
+
+        edited_at = await self._fetch_field("lastEditedAt")
+
+        if edited_at is None:
+            return None
+
+        if TYPE_CHECKING:
+            edited_at = cast(str, edited_at)
+
+        return github.utility.iso_to_datetime(edited_at)
+
+    async def fetch_published_at(
+        self: Self,
+        /,
+    ) -> DateTime | None:
+        """
+        |coro|
+
+        Fetches the date and time at which the comment was published.
+
+
+        Raises
+        ------
+
+        ~github.core.errors.ClientObjectMissingFieldError
+            The :attr:`id` attribute is missing.
+
+
+        :rtype: :class:`~datetime.datetime` | None
+        """
+
+        if TYPE_CHECKING and not isinstance(self, Node):
+            raise NotImplementedError
+
+        published_at = await self._fetch_field("publishedAt")
+
+        if published_at is None:
+            return None
+
+        if TYPE_CHECKING:
+            published_at = cast(str, published_at)
+
+        return github.utility.iso_to_datetime(published_at)
+
+    async def fetch_updated_at(
+        self: Self,
+        /,
+    ) -> DateTime:
+        """
+        |coro|
+
+        Fetches the date and time at which the comment was last
+        updated.
+
+
+        Raises
+        ------
+
+        ~github.core.errors.ClientObjectMissingFieldError
+            The :attr:`id` attribute is missing.
+
+
+        :rtype: :class:`~datetime.datetime`
+        """
+
+        if TYPE_CHECKING and not isinstance(self, Node):
+            raise NotImplementedError
+
+        updated_at = await self._fetch_field("updatedAt")
+
+        if TYPE_CHECKING:
+            updated_at = cast(str, updated_at)
+
+        return github.utility.iso_to_datetime(updated_at)
+
+    async def fetch_viewer_is_author(
+        self: Self,
+        /,
+    ) -> bool:
+        """
+        |coro|
+
+        Fetches whether the authenticated user authored the comment.
+
+        :rtype: :class:`bool`
+        """
+
+        return await self._fetch_field("viewerDidAuthor")  # type: ignore
 
 
 __all__ = [
