bcc0b0ead5663d13ff20fee1ae4520b8db12f6c5
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2024-12-31 17:50:10 +0000

    implement Labelable.fetch_labels

--- github/core/http.py
+++ github/core/http.py
@@ -932,6 +932,26 @@ class HTTPClient(graphql.client.http.HTTPClient):
 
         return data
 
+    async def collect_labelable_labels(
+        self: Self,
+        /,
+        labelable_id: str,
+        order_by: str | None,
+        *,
+        fields: Iterable[str] = MISSING,
+        **kwargs,
+    ) -> ConnectionData[LabelData]:
+        fields = github.utility.get_merged_graphql_fields(github.Label, fields)
+        query = "query($after:String,$before:String,$first:Int,$last:Int,$order_by:LabelOrder,$labelable_id:ID!){node(id:$labelable_id){...on Labelable{labels(after:$after,before:$before,first:$first,last:$last,orderBy:$order_by){nodes{%s},pageInfo{endCursor,hasNextPage,hasPreviousPage,startCursor}}}}}" % ",".join(fields)
+        path = ("node", "labels")
+
+        if order_by is None:
+            order_by_data = None
+        else:
+            order_by_data = {"direction": "ASC", "field": order_by}
+
+        return await self._collect(query, *path, labelable_id=labelable_id, order_by=order_by_data, **kwargs)
+
     async def collect_repository_assignable_users(
         self: Self,
         /,
--- github/interfaces/labelable.py
+++ github/interfaces/labelable.py
@@ -4,6 +4,13 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from typing_extensions import Self
 
+    from github.connection import Connection, LabelOrder
+    from github.interfaces import Node
+    from github.repository import Label
+
+import github
+from github.utility import MISSING
+
 
 if TYPE_CHECKING:
     from typing import TypedDict
@@ -59,6 +66,62 @@ class Labelable:
         """
 
         return await self._fetch_field("viewerCanLabel")  # type: ignore
+
+    def fetch_labels(
+        self: Self,
+        /,
+        *,
+        cursor: str | None = MISSING,
+        limit: int = MISSING,
+        order_by: LabelOrder = MISSING,
+        reverse: bool = MISSING,
+        **kwargs,  # TODO
+    ) -> Connection[Label]:
+        """
+        |aiter|
+
+        Fetches labels from the labelable.
+
+
+        Parameters
+        ----------
+
+        cursor: :class:`str`
+            The cursor to start at.
+        limit: :class:`int`
+            The maximum number of elements to yield.
+        order_by: :class:`~github.LabelOrder`
+            The field by which to order the elements.
+        reverse: :class:`bool`
+            Whether to yield the elements in reverse order.
+
+
+        Raises
+        ------
+
+        ~github.core.errors.ClientObjectMissingFieldError
+            The :attr:`id` attribute is missing.
+
+
+        :rtype: :class:`~github.Connection`[:class:`~github.Label`]
+        """
+
+        if TYPE_CHECKING and not isinstance(self, Node):
+            raise NotImplementedError
+
+        def labeldata_to_label(labeldata: LabelData, /) -> Label:
+            return github.Label._from_data(labeldata, http=self._http)
+
+        return github.Connection(
+            self._http.collect_labelable_labels,
+            self.id,
+            order_by.value if order_by is not MISSING else None,
+            data_map=labeldata_to_label,
+            cursor=cursor if cursor is not MISSING else None,
+            limit=limit if limit is not MISSING else None,
+            reverse=reverse if reverse is not MISSING else False,
+            **kwargs,
+        )
 
 
 __all__ = [
