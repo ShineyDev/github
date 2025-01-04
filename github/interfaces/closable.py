e02efcc26d0e0e60f4682edafab1a2a80634d06e
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2025-01-04 15:30:28 +0000

    implement fetch_* for closable fields

--- github/interfaces/closable.py
+++ github/interfaces/closable.py
@@ -2,8 +2,10 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from typing import cast
     from typing_extensions import Self
 
+    from github.interfaces import Node
     from github.utility.types import DateTime
 
 import github
@@ -92,6 +94,82 @@ class Closable:
         """
 
         return self._data["viewerCanReopen"]
+
+    async def fetch_closed_at(
+        self: Self,
+        /,
+    ) -> DateTime | None:
+        """
+        |coro|
+
+        Fetches the date and time at which the closable was closed, if
+        any.
+
+
+        Raises
+        ------
+
+        ~github.core.errors.ClientObjectMissingFieldError
+            The :attr:`id` attribute is missing.
+
+
+        :rtype: :class:`~datetime.datetime` | None
+        """
+
+        if TYPE_CHECKING and not isinstance(self, Node):
+            raise NotImplementedError
+
+        closed_at = await self._fetch_field("closedAt")
+
+        if closed_at is None:
+            return None
+
+        if TYPE_CHECKING:
+            closed_at = cast(str, closed_at)
+
+        return github.utility.iso_to_datetime(closed_at)
+
+    async def fetch_is_closed(
+        self: Self,
+        /,
+    ) -> bool:
+        """
+        |coro|
+
+        Fetches whether the closable is closed.
+
+        :rtype: :class:`bool`
+        """
+
+        return await self._fetch_field("closed")  # type: ignore
+
+    async def fetch_viewer_can_close(
+        self: Self,
+        /,
+    ) -> bool:
+        """
+        |coro|
+
+        Fetches whether the authenticated user can close the closable.
+
+        :rtype: :class:`bool`
+        """
+
+        return await self._fetch_field("viewerCanClose")  # type: ignore
+
+    async def fetch_viewer_can_reopen(
+        self: Self,
+        /,
+    ) -> bool:
+        """
+        |coro|
+
+        Fetches whether the authenticated user can reopen the closable.
+
+        :rtype: :class:`bool`
+        """
+
+        return await self._fetch_field("viewerCanReopen")  # type: ignore
 
 
 __all__ = [
