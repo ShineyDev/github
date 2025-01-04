ff5dcb167172153addd1ebf854d483cda53b3123
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2025-01-04 20:03:04 +0000

    implement User.fetch_watching

--- github/core/http.py
+++ github/core/http.py
@@ -1307,6 +1307,26 @@ class HTTPClient(graphql.client.http.HTTPClient):
 
         return await self._collect(query, *path, user_id=user_id, order_by=order_by_data, **kwargs)
 
+    async def collect_user_watching(
+        self: Self,
+        /,
+        user_id: str,
+        order_by: str | None,
+        *,
+        fields: Iterable[str] = MISSING,
+        **kwargs,
+    ) -> ConnectionData[RepositoryData]:
+        fields = github.utility.get_merged_graphql_fields(github.Repository, fields)
+        query = "query($after:String,$before:String,$first:Int,$last:Int,$order_by:RepositoryOrder,$user_id:ID!){node(id:$user_id){... on User{watching(after:$after,before:$before,first:$first,last:$last,orderBy:$order_by){nodes{%s},pageInfo{endCursor,hasNextPage,hasPreviousPage,startCursor}}}}}" % ",".join(fields)
+        path = ("node", "watching")
+
+        if order_by is None:
+            order_by_data = None
+        else:
+            order_by_data = {"direction": "ASC", "field": order_by}
+
+        return await self._collect(query, *path, user_id=user_id, order_by=order_by_data, **kwargs)
+
     async def _mutate(
         self: Self,
         document_: str,
--- github/user/user.py
+++ github/user/user.py
@@ -5,10 +5,10 @@ if TYPE_CHECKING:
     from typing import cast
     from typing_extensions import Self
 
-    from github.connection import Connection, IssueOrder, OrganizationOrder, PullOrder
+    from github.connection import Connection, IssueOrder, OrganizationOrder, PullOrder, RepositoryOrder
     from github.organization import Organization
     from github.organization.organization import OrganizationData
-    from github.repository import Issue, Pull
+    from github.repository import Issue, Pull, Repository
     from github.repository.issue import IssueData
     from github.repository.pull import PullData
     from github.user import UserStatus
@@ -1304,6 +1304,56 @@ class User(
             self.id,
             order_by.value if order_by is not MISSING else None,
             data_map=pulldata_to_pull,
+            cursor=cursor if cursor is not MISSING else None,
+            limit=limit if limit is not MISSING else None,
+            reverse=reverse if reverse is not MISSING else False,
+        )
+
+    def fetch_watching(
+        self: Self,
+        /,
+        *,
+        cursor: str | None = MISSING,
+        limit: int = MISSING,
+        order_by: RepositoryOrder = MISSING,
+        reverse: bool = MISSING,
+    ) -> Connection[Repository]:
+        """
+        |aiter|
+
+        Fetches repositories the user is watching.
+
+
+        Parameters
+        ----------
+        cursor: :class:`str`
+            The cursor to start at.
+        limit: :class:`int`
+            The maximum number of elements to yield.
+        order_by: :class:`~github.RepositoryOrder`
+            The field by which to order the elements.
+        reverse: :class:`bool`
+            Whether to yield the elements in reverse order.
+
+
+        Raises
+        ------
+
+        ~github.core.errors.ClientObjectMissingFieldError
+            The :attr:`id` attribute is missing.
+
+
+        :rtype: :class:`~github.Connection`[:class:`~github.Repository`]
+        """
+
+        def repositorydata_to_repository(repositorydata: RepositoryData, /) -> Repository:
+            return github.Repository._from_data(repositorydata, http=self._http)
+
+        return github.Connection(
+            self._http.collect_user_watching,
+            self.id,
+            order_by.value if order_by is not MISSING else None,
+            data_map=repositorydata_to_repository,
             cursor=cursor if cursor is not MISSING else None,
             limit=limit if limit is not MISSING else None,
             reverse=reverse if reverse is not MISSING else False,
