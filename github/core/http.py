9146330044ccf1341d4d9eea957e2f61ddfda955
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2025-01-05 16:08:37 +0000

    implement Repository.fetch_issues

--- github/core/http.py
+++ github/core/http.py
@@ -1153,6 +1153,26 @@ class HTTPClient(graphql.client.http.HTTPClient):
 
         return await self._collect(query, *path, repository_id=repository_id, order_by=order_by_data, **kwargs)
 
+    async def collect_repository_issues(
+        self: Self,
+        /,
+        repository_id: str,
+        order_by: str | None,
+        *,
+        fields: Iterable[str] = MISSING,
+        **kwargs,
+    ) -> ConnectionData[IssueData]:
+        fields = github.utility.get_merged_graphql_fields(github.Issue, fields)
+        query = "query($after:String,$before:String,$first:Int,$last:Int,$order_by:IssueOrder,$repository_id:ID!){node(id:$repository_id){...on Repository{issues(after:$after,before:$before,first:$first,last:$last,orderBy:$order_by){nodes{%s},pageInfo{endCursor,hasNextPage,hasPreviousPage,startCursor}}}}}" % ",".join(fields)
+        path = ("node", "issues")
+
+        if order_by is None:
+            order_by_data = None
+        else:
+            order_by_data = {"direction": "ASC", "field": order_by}
+
+        return await self._collect(query, *path, repository_id=repository_id, order_by=order_by_data, **kwargs)
+
     async def collect_repository_labels(
         self: Self,
         /,
--- github/repository/repository.py
+++ github/repository/repository.py
@@ -5,11 +5,12 @@ if TYPE_CHECKING:
     from typing import Literal, cast
     from typing_extensions import Self
 
-    from github.connection import Connection, DiscussionOrder, LabelOrder, RepositoryOrder
+    from github.connection import Connection, DiscussionOrder, IssueOrder, LabelOrder, RepositoryOrder
     from github.content import CodeOfConduct, License
     from github.organization import Organization
     from github.repository import Discussion, Issue, Label, Topic
     from github.repository.discussion import DiscussionData
+    from github.repository.issue import IssueData
     from github.user import User
     from github.utility.types import DateTime
 
@@ -1685,6 +1686,58 @@ class Repository(
             self.id,
             order_by.value if order_by is not MISSING else None,
             data_map=repositorydata_to_repository,
+            cursor=cursor if cursor is not MISSING else None,
+            limit=limit if limit is not MISSING else None,
+            reverse=reverse if reverse is not MISSING else False,
+            **kwargs,
+        )
+
+    def fetch_issues(
+        self: Self,
+        /,
+        *,
+        cursor: str | None = MISSING,
+        limit: int = MISSING,
+        order_by: IssueOrder = MISSING,
+        reverse: bool = MISSING,
+        **kwargs,  # TODO
+    ) -> Connection[Issue]:
+        """
+        |aiter|
+
+        Fetches issues in the repository.
+
+
+        Parameters
+        ----------
+        cursor: :class:`str`
+            The cursor to start at.
+        limit: :class:`int`
+            The maximum number of elements to yield.
+        order_by: :class:`~github.IssueOrder`
+            The field by which to order the elements.
+        reverse: :class:`bool`
+            Whether to yield the elements in reverse order.
+
+
+        Raises
+        ------
+
+        ~github.core.errors.ClientObjectMissingFieldError
+            The :attr:`id` attribute is missing.
+
+
+        :rtype: :class:`~github.Connection`[:class:`~github.Issue`]
+        """
+
+        def issuedata_to_issue(issuedata: IssueData, /) -> Issue:
+            return github.Issue._from_data(issuedata, http=self._http)
+
+        return github.Connection(
+            self._http.collect_repository_issues,
+            self.id,
+            order_by.value if order_by is not MISSING else None,
+            data_map=issuedata_to_issue,
             cursor=cursor if cursor is not MISSING else None,
             limit=limit if limit is not MISSING else None,
             reverse=reverse if reverse is not MISSING else False,
