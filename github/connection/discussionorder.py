401141367a386b3b92c35dc74ddf5a4e497a6c80
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2025-01-05 15:47:52 +0000

    implement DiscussionOrder

---
+++ github/connection/discussionorder.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+import enum
+
+
+if TYPE_CHECKING:
+    from typing import Literal
+    from typing_extensions import TypeAlias
+
+
+    DiscussionOrderData: TypeAlias = Literal["CREATED_AT", "UPDATED_AT"]
+
+
+class DiscussionOrder(enum.Enum):
+    """
+    Represents fields by which you can order :class:`discussions <github.Discussion>`.
+    """
+
+    #: The date and time at which the discussion was created.
+    created_at = "CREATED_AT"
+
+    #: The date and time at which the discussion was last updated.
+    updated_at = "UPDATED_AT"
+
+
+__all__ = [
+    "DiscussionOrder"
+]
