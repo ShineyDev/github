1129e28334e9888e2cda62b262cdb8f66eded0c7
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2025-01-05 19:05:33 +0000

    implement DiscussionState

--- github/repository/__init__.py
+++ github/repository/__init__.py
@@ -1,5 +1,7 @@
 from github.repository.discussion import *
 from github.repository.discussion import __all__ as _discussion__all__
+from github.repository.discussionstate import *
+from github.repository.discussionstate import __all__ as _discussionstate__all__
 from github.repository.issue import *
 from github.repository.issue import __all__ as _issue__all__
 from github.repository.issuestate import *
@@ -20,6 +22,7 @@ from github.repository.topic import __all__ as _topic__all__
 
 __all__: list[str] = [  # type: ignore[reportUnsupportedDunderAll]
     *_discussion__all__,
+    *_discussionstate__all__,
     *_issue__all__,
     *_issuestate__all__,
     *_label__all__,
---
+++ github/repository/discussionstate.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+import enum
+
+
+if TYPE_CHECKING:
+    from typing import Literal
+    from typing_extensions import TypeAlias
+
+
+    DiscussionStateData: TypeAlias = Literal["CLOSED", "OPEN"]
+
+
+class DiscussionState(enum.Enum):
+    """
+    Represents the state of a :class:`discussion <github.Discussion>`.
+    """
+
+    #: The discussion is closed.
+    closed = "CLOSED"
+
+    #: The discussion is open.
+    open = "OPEN"
+
+
+__all__ = [
+    "DiscussionState"
+]
