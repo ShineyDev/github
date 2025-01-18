9abce128aea1358f90eaad70fbcba262d0ce4c00
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2025-01-18 14:11:00 +0000

    implement AdvisorySeverity

--- github/security/__init__.py
+++ github/security/__init__.py
@@ -2,9 +2,12 @@ from github.security.advisory import *
 from github.security.advisory import __all__ as _advisory__all__
 from github.security.advisoryclassification import *
 from github.security.advisoryclassification import __all__ as _advisoryclassification__all__
+from github.security.advisoryseverity import *
+from github.security.advisoryseverity import __all__ as _advisoryseverity__all__
 
 
 __all__: list[str] = [  # type: ignore[reportUnsupportedDunderAll]
     *_advisory__all__,
     *_advisoryclassification__all__,
+    *_advisoryseverity__all__,
 ]
---
+++ github/security/advisoryseverity.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+import enum
+
+
+if TYPE_CHECKING:
+    from typing import Literal
+    from typing_extensions import TypeAlias
+
+
+    AdvisorySeverityData: TypeAlias = Literal["CRITICAL", "HIGH", "LOW", "MODERATE"]
+
+
+class AdvisorySeverity(enum.Enum):
+    """
+    Represents the severity of an :class:`~github.Advisory`.
+    """
+
+    #: The advisory severity is critical.
+    critical = "CRITICAL"
+
+    #: The advisory severity is high.
+    high = "HIGH"
+
+    #: The advisory severity is moderate.
+    moderate = "MODERATE"
+
+    #: The advisory severity is low.
+    low = "LOW"
+
+
+__all__ = [
+    "AdvisorySeverity"
+]
