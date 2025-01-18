cb1d43da53793da66ac04674e80d71a36265afd4
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2025-01-18 14:09:10 +0000

    implement AdvisoryClassification

--- github/security/__init__.py
+++ github/security/__init__.py
@@ -1,7 +1,10 @@
 from github.security.advisory import *
 from github.security.advisory import __all__ as _advisory__all__
+from github.security.advisoryclassification import *
+from github.security.advisoryclassification import __all__ as _advisoryclassification__all__
 
 
 __all__: list[str] = [  # type: ignore[reportUnsupportedDunderAll]
     *_advisory__all__,
+    *_advisoryclassification__all__,
 ]
---
+++ github/security/advisoryclassification.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+import enum
+
+
+if TYPE_CHECKING:
+    from typing import Literal
+    from typing_extensions import TypeAlias
+
+
+    AdvisoryClassificationData: TypeAlias = Literal["GENERAL", "MALWARE"]
+
+
+class AdvisoryClassification(enum.Enum):
+    """
+    Represents the classification of an :class:`~github.Advisory`.
+    """
+
+    #: The advisory classification is general.
+    general = "GENERAL"
+
+    #: The advisory classification is malware.
+    malware = "MALWARE"
+
+
+__all__ = [
+    "AdvisoryClassification"
+]
