faf1fa95aaffce11a825ea3e44479f121becd785
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2025-01-19 13:27:45 +0000

    implement Client.fetch_vulnerabilities

--- github/core/client.py
+++ github/core/client.py
@@ -7,9 +7,11 @@ if TYPE_CHECKING:
     from aiohttp import ClientSession
 
     from github.api import Metadata, RateLimit
+    from github.connection import Connection, VulnerabilityOrder
     from github.content import CodeOfConduct, License
     from github.organization import Organization
     from github.repository import Repository, Topic
+    from github.security import Vulnerability
     from github.user import AuthenticatedUser, User, UserStatus
     from github.utility.types import DateTime, T_json_object
 
@@ -496,6 +498,54 @@ class Client(graphql.client.Client):
 
         data = await self._http.fetch_query_viewer(**kwargs)
         return github.AuthenticatedUser._from_data(data, http=self._http)
+
+    def fetch_vulnerabilities(
+        self: Self,
+        /,
+        *,
+        cursor: str | None = MISSING,
+        limit: int = MISSING,
+        order_by: VulnerabilityOrder = MISSING,
+        reverse: bool = MISSING,
+        **kwargs,  # TODO
+    ) -> Connection[Vulnerability]:
+        """
+        |aiter|
+
+        Fetches individual vulnerabilities.
+
+
+        Parameters
+        ----------
+        cursor: :class:`str`
+            The cursor to start at.
+        limit: :class:`int`
+            The maximum number of elements to yield.
+        order_by: :class:`~github.VulnerabilityOrder`
+            The field by which to order the elements.
+        reverse: :class:`bool`
+            Whether to yield the elements in reverse order.
+
+
+        Raises
+        ------
+
+        ~github.core.errors.ClientObjectMissingFieldError
+            The :attr:`id` attribute is missing.
+
+
+        :rtype: :class:`~github.Connection`[:class:`~github.Vulnerability`]
+        """
+
+        return github.Connection(
+            self._http.collect_query_vulnerabilities,
+            order_by.value if order_by is not MISSING else None,
+            data_map=lambda d: github.Vulnerability._from_data(d),
+            cursor=cursor if cursor is not MISSING else None,
+            limit=limit if limit is not MISSING else None,
+            reverse=reverse if reverse is not MISSING else False,
+            **kwargs,
+        )
 
     async def clear_status(
         self: Self,
