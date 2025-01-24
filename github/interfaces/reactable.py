a1d2ebc4bb2f98a399d83be776824b0534606489
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2025-01-24 15:46:10 +0000

    implement Reactable.reaction_count

--- github/interfaces/reactable.py
+++ github/interfaces/reactable.py
@@ -4,6 +4,8 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from typing_extensions import Self
 
+    from github.interfaces import Node
+
 
 if TYPE_CHECKING:
     from typing import TypedDict
@@ -29,8 +31,22 @@ class Reactable:
     _data: ReactableData
 
     _graphql_fields = {
+        "reaction_count": "reactions{totalCount}",
         "viewer_can_react": "viewerCanReact",
     }
+
+    @property
+    def reaction_count(
+        self: Self,
+        /,
+    ) -> int:
+        """
+        The number of reactions on the reactable.
+
+        :type: :class:`int`
+        """
+
+        return self._data["reactions"]["totalCount"]
 
     @property
     def viewer_can_react(
@@ -44,6 +60,31 @@ class Reactable:
         """
 
         return self._data["viewerCanReact"]
+
+    async def fetch_reaction_count(
+        self: Self,
+        /,
+    ) -> int:
+        """
+        |coro|
+
+        Fetches the number of reactions on the reactable.
+
+
+        Raises
+        ------
+
+        ~github.core.errors.ClientObjectMissingFieldError
+            The :attr:`id` attribute is missing.
+
+
+        :rtype: :class:`int`
+        """
+
+        if TYPE_CHECKING and not isinstance(self, Node):
+            raise NotImplementedError
+
+        return await self._fetch_field("reactions{totalCount}")  # type: ignore
 
     async def fetch_viewer_can_react(
         self: Self,
