85101aace8ddf8d9d32aac9a81b680d48f7e5980
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2025-01-25 17:20:16 +0000

    implement PackageOwner.package_count

--- github/interfaces/packageowner.py
+++ github/interfaces/packageowner.py
@@ -4,6 +4,8 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from typing_extensions import Self
 
+    from github.interfaces import Node
+
 
 if TYPE_CHECKING:
     from typing import TypedDict
@@ -24,6 +26,48 @@ class PackageOwner:
     __slots__ = ()
 
     _data: PackageOwnerData
+
+    _graphql_fields = {
+        "package_count": "packages{totalCount}",
+    }
+
+    @property
+    def package_count(
+        self: Self,
+        /,
+    ) -> int:
+        """
+        The number of packages on the package owner.
+
+        :type: :class:`int`
+        """
+
+        return self._data["packages"]["totalCount"]
+
+    async def fetch_package_count(
+        self: Self,
+        /,
+    ) -> int:
+        """
+        |coro|
+
+        Fetches the number of packages on the package owner.
+
+
+        Raises
+        ------
+
+        ~github.core.errors.ClientObjectMissingFieldError
+            The :attr:`id` attribute is missing.
+
+
+        :rtype: :class:`int`
+        """
+
+        if TYPE_CHECKING and not isinstance(self, Node):
+            raise NotImplementedError
+
+        return await self._fetch_field("packages{totalCount}")  # type: ignore
 
 
 __all__: list[str] = [
