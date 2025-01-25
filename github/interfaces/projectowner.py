b624cc19fe6f8e097a02e1d7306604d27f3bf63d
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2025-01-25 17:25:21 +0000

    implement ProjectOwner.project_count

--- github/interfaces/projectowner.py
+++ github/interfaces/projectowner.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
+if TYPE_CHECKING:
+    from github.interfaces import Node
+
 
 if TYPE_CHECKING:
     from typing import TypedDict
@@ -22,6 +25,48 @@ class ProjectOwner:
     __slots__ = ()
 
     _data: ProjectOwnerData
+
+    _graphql_fields = {
+        "project_count": "projectsV2{totalCount}",
+    }
+
+    @property
+    def project_count(
+        self,
+        /,
+    ) -> int:
+        """
+        The number of projects on the project owner.
+
+        :type: :class:`int`
+        """
+
+        return self._data["projectsV2"]["totalCount"]
+
+    async def fetch_project_count(
+        self,
+        /,
+    ) -> int:
+        """
+        |coro|
+
+        Fetches the number of projects on the project owner.
+
+
+        Raises
+        ------
+
+        ~github.core.errors.ClientObjectMissingFieldError
+            The :attr:`id` attribute is missing.
+
+
+        :rtype: :class:`int`
+        """
+
+        if TYPE_CHECKING and not isinstance(self, Node):
+            raise NotImplementedError
+
+        return await self._fetch_field("projectsV2{totalCount}")  # type: ignore
 
 
 __all__: list[str] = [
