12ae8068f10f9b513f803d04f0312fb21853c7b1
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2025-01-27 20:38:58 +0000

    implement CloseEvent (ClosedEvent) type
    2/56

--- github/timeline/__init__.py
+++ github/timeline/__init__.py
@@ -1,7 +1,10 @@
 from github.timeline.assigneeaddevent import *
 from github.timeline.assigneeaddevent import __all__ as _assigneeaddevent__all__
+from github.timeline.closeevent import *
+from github.timeline.closeevent import __all__ as _closeevent__all__
 
 
 __all__ = [  # type: ignore[reportUnsupportedDunderAll]
     *_assigneeaddevent__all__,
+    *_closeevent__all__,
 ]
---
+++ github/timeline/closeevent.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from github.interfaces import Node, Resource, TimelineItem, Type
+
+
+if TYPE_CHECKING:
+    from typing import Literal
+
+    from github.interfaces.node import NodeData
+    from github.interfaces.resource import ResourceData
+    from github.interfaces.timelineitem import TimelineItemData
+    from github.interfaces.type import TypeData
+    from github.repository.issue import IssueData
+    from github.repository.pull import PullData
+
+
+    class CloseEventData(NodeData, ResourceData, TimelineItemData, TypeData):
+        __typename: Literal["ClosedEvent"]
+
+        closable: IssueData | PullData
+        # closer  # TODO
+        stateReason: Literal["COMPLETED", "DUPLICATE", "NOT_PLANNED", "REOPENED"]
+
+
+class CloseEvent(Node, Resource, TimelineItem, Type):
+    """
+    Represents a close event on an :class:`~github.Issue` or
+    :class:`~github.Pull` timeline.
+
+
+    .. container:: operations
+
+        .. describe:: x == y
+        .. describe:: x != y
+
+            Compares two objects by their :attr:`ID <.id>`.
+
+        .. describe:: hash(x)
+
+            Returns the hash of the object's :attr:`ID <.id>`.
+    """
+
+    __slots__ = ()
+
+    _data: CloseEventData
+
+    _node_prefix = "CE"
+
+
+__all__ = [
+    "CloseEvent",
+]
