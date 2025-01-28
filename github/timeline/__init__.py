a445b318d3545c470f6636146122a1e4250dc0ce
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2025-01-28 12:57:12 +0000

    implement ReopenEvent (ReopenedEvent) type
    4/56

--- github/timeline/__init__.py
+++ github/timeline/__init__.py
@@ -4,10 +4,13 @@ from github.timeline.assigneeremoveevent import *
 from github.timeline.assigneeremoveevent import __all__ as _assigneeremoveevent__all__
 from github.timeline.closeevent import *
 from github.timeline.closeevent import __all__ as _closeevent__all__
+from github.timeline.reopenevent import *
+from github.timeline.reopenevent import __all__ as _reopenevent__all__
 
 
 __all__ = [  # type: ignore[reportUnsupportedDunderAll]
     *_assigneeaddevent__all__,
     *_assigneeremoveevent__all__,
     *_closeevent__all__,
+    *_reopenevent__all__,
 ]
