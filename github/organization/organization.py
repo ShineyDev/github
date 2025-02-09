071c39e22cbdb54fd83dbc8589dc4b3707b7abee
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2025-02-09 14:59:26 +0000

    remove Organization.mannequin_count

--- github/organization/organization.py
+++ github/organization/organization.py
@@ -160,7 +160,7 @@ class Organization(
         "is_verified": "isVerified",
         "is_viewer_following": "viewerIsFollowing",
         "is_viewer_member": "viewerIsAMember",
-        "mannequin_count": "mannequins{totalCount}",
+        # "mannequin_count": "mannequins{totalCount}",  # NOTE: throws FORBIDDEN in non-enterprise organizations
         "team_count": "teams{totalCount}",
         "twitter_username": "twitterUsername",
         "updated_at": "updatedAt",
@@ -310,19 +310,6 @@ class Organization(
         """
 
         return self._data["viewerIsAMember"]
-
-    @property
-    def mannequin_count(
-        self,
-        /,
-    ) -> int:
-        """
-        The number of mannequins in the organization.
-
-        :type: :class:`int`
-        """
-
-        return self._data["mannequins"]["totalCount"]
 
     @property
     def team_count(
@@ -602,28 +589,6 @@ class Organization(
         """
 
         return await self._fetch_field("viewerIsAMember")  # type: ignore
-
-    async def fetch_mannequin_count(
-        self,
-        /,
-    ) -> int:
-        """
-        |coro|
-
-        Fetches the number of mannequins in the organization.
-
-
-        Raises
-        ------
-
-        ~github.core.errors.ClientObjectMissingFieldError
-            The :attr:`id` attribute is missing.
-
-
-        :rtype: :class:`int`
-        """
-
-        return await self._fetch_field("mannequins{totalCount}")  # type: ignore
 
     async def fetch_team_count(
         self,
