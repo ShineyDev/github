912f8ed3b003e72a4a13840ea0eb2b8e055b9145
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2025-02-10 13:53:30 +0000

    implement ParentRemoveEvent
    ParentIssueRemovedEvent type

--- github/timeline/__init__.py
+++ github/timeline/__init__.py
@@ -12,6 +12,8 @@ from github.timeline.lockevent import *
 from github.timeline.lockevent import __all__ as _lockevent__all__
 from github.timeline.parentaddevent import *
 from github.timeline.parentaddevent import __all__ as _parentaddevent__all__
+from github.timeline.parentremoveevent import *
+from github.timeline.parentremoveevent import __all__ as _parentremoveevent__all__
 from github.timeline.pinevent import *
 from github.timeline.pinevent import __all__ as _pinevent__all__
 from github.timeline.reopenevent import *
@@ -34,6 +36,7 @@ __all__ = [  # type: ignore[reportUnsupportedDunderAll]
     *_labelremoveevent__all__,
     *_lockevent__all__,
     *_parentaddevent__all__,
+    *_parentremoveevent__all__,
     *_pinevent__all__,
     *_reopenevent__all__,
     *_subscribeevent__all__,
---
+++ github/timeline/parentremoveevent.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from github.interfaces import Node, TimelineItem, Type
+
+
+if TYPE_CHECKING:
+    from typing import Literal
+
+    from github.interfaces.node import NodeData
+    from github.interfaces.timelineitem import TimelineItemData
+    from github.interfaces.type import TypeData
+    from github.repository.issue import IssueData
+
+
+    class ParentRemoveEventData(NodeData, TimelineItemData, TypeData):
+        __typename: Literal["ParentIssueRemovedEvent"]
+
+        parent: IssueData | None
+
+
+class ParentRemoveEvent(Node, TimelineItem, Type):
+    """
+    Represents a parent removed event on an :class:`~github.Issue`
+    timeline.
+
+
+    .. container:: operations
+
+        .. describe:: x == y
+        .. describe:: x != y
+
+            Compares two objects by their :attr:`ID <.id>`.
+
+        .. describe:: hash(x)
+
+            Returns the hash of the object's :attr:`ID <.id>`.
+    """
+
+    __slots__ = ()
+
+    _data: ParentRemoveEventData
+
+    _graphql_type = "ParentIssueRemovedEvent"
+
+    _node_prefix = "PIRE"
+
+
+__all__ = [
+    "ParentRemoveEvent",
+]
